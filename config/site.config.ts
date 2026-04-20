f2879e6f65387837b2212b9dd3e355f1f2998857
Zhen Huang <193303856+zhenhuang-tw@users.noreply.github.com>
2026-04-20 20:08:50 +0800

    chore(site): update site name

--- config/site.config.ts
+++ config/site.config.ts
@@ -1,7 +1,7 @@
 // config/site.config.ts
 export const siteConfig = {
-  name: 'NTPU Student Congress Cloud',
-  shortName: 'NTPUSC Cloud',
+  name: 'NTPUSCS Legislative Hub',
+  shortName: 'ntpuscs LegiHub',
   orgName: '國立臺北大學學生議會（三峽校區）',
   orgNameEn: 'NTPU Student Congress (Sanxia)',
   orgUrl: 'https://cloud.ntpusu.org/',
